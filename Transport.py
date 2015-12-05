3e620edb33448a547ed5c99c76f61562bb87c183
chomi87 <michael.chong.bo@gmail.com>
2015-12-05 23:39:07 +0100

    Plot triplo

--- Transport.py
+++ Transport.py
@@ -5,7 +5,7 @@
 
 ### Libraries
 
-# In[8]:
+# In[97]:
 
 import numpy
 import math
@@ -14,7 +14,7 @@ import matplotlib.pyplot as plt
 
 ### Functions
 
-# In[15]:
+# In[98]:
 
 def lorentz(E,W,V):
     lor=1/(math.pi*(1+((V-E)/W)**2))
@@ -22,7 +22,7 @@ def lorentz(E,W,V):
     return lor
 
 
-# In[16]:
+# In[99]:
 
 def dos_electrodes(bias,a_t,a_s,V):
     n_steps=V.size
@@ -37,28 +37,31 @@ def dos_electrodes(bias,a_t,a_s,V):
     return dos
 
 
-# In[60]:
+# In[100]:
 
 def dos_electrodes2(bias,a_t,a_s,V):
     n_steps=V.size
     dos=numpy.zeros(n_steps)
     e_tip=0
     e_sample=0
+    K=8.6*math.pow(10, -5)
+    T=4.7
+    KT=K*T
     
     e_tip=e_tip-(bias*a_t)
     e_sample=e_sample+(bias*a_s)
     
-    dos=1/(numpy.exp((V-e_tip)/0.0001)+1)
+    dos=[1/(numpy.exp((V-e_tip)/KT)+1),1/(numpy.exp((V-e_sample)/KT)+1)]
     return dos
 
 
 ### Parameters
 
-# In[51]:
+# In[101]:
 
 V_min=-2
 V_max=2
-Step_size=0.01
+Step_size=0.001
 V=numpy.arange(V_min,V_max,Step_size)
 n_steps=V.size
 I=numpy.zeros(n_steps)
@@ -67,7 +70,7 @@ dos=numpy.zeros(n_steps)
 
 #### Molecule
 
-# In[18]:
+# In[102]:
 
 E_homo=+0.5
 E_lumo=-0.1
@@ -75,7 +78,7 @@ W_homo=0.01
 W_lumo=0.01
 
 
-# In[19]:
+# In[103]:
 
 dos=numpy.zeros(n_steps)
 dos=dos+lorentz(E_homo,W_homo,V)
@@ -84,26 +87,59 @@ dos=dos+lorentz(E_lumo,W_lumo,V)
 
 #### Junction
 
-# In[20]:
+# In[107]:
 
 Alpha_1=0.3
 Alpha_2=1-Alpha_1
 Gamma_1=1
 Gamma_2=0.001
 E_block=0
+electrodes=dos_electrodes2(0,Alpha_1,Alpha_2,V)
 
 
 ### Main
 
-# In[63]:
+# In[109]:
 
-tip=dos_electrodes2(1,Alpha_1,Alpha_2,V)
-plt.plot(V,tip)
-plt.fill_between(V,tip,0,color='0.8')
+f, axarr = plt.subplots(3, sharex=True)
+axarr[0].plot(V, dos, color='r')
+axarr[0].fill_between(V,dos,0,where=V<0,color='r')
+
+axarr[0].set_title('Molecule dos')
+
+axarr[1].plot(V, electrodes[0],color='g')
+axarr[1].fill_between(V,electrodes[0],0,color='g')
+axarr[1].set_title('Tip dos')
+
+axarr[2].plot(V, electrodes[1],color='b')
+axarr[2].fill_between(V,electrodes[1],0,color='b')
+axarr[2].set_title('Sample dos')
+
+
 plt.show()
 
 
-# In[62]:
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[96]:
+
+
+
+
+# In[96]:
+
+
+
+
+# In[77]:
 
 
 
